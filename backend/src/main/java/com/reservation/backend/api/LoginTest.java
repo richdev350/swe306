7c325467fd7dc32cdbda0eced0b02c74d5b06016
bigstar888 <goldtrump1015@gmail.com>
2021-12-03 10:51:08 +0800

    api: test api login example

---
+++ backend/src/main/java/com/reservation/backend/api/LoginTest.java
@@ -0,0 +1,47 @@
+package com.reservation.backend.api;
+
+import com.alibaba.fastjson.JSONObject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.*;
+
+@WebServlet(name = "LoginTest", value = "/api/test/login")
+public class LoginTest extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("abcdef");
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // an json example
+        // https://www.liaoxuefeng.com/wiki/1252599548343744/1320418650619938
+        // https://www.runoob.com/w3cnote/fastjson-intro.html
+        JSONObject respObj = new JSONObject();
+
+        respObj.put("abc", "cba");
+
+        resp.setContentType("application/json");
+
+        // Hello
+        PrintWriter out = resp.getWriter();
+        out.println(respObj);
+
+    }
+
+    public void destroy() {
+    }
+}
