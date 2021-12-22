1df784453dd214977dd0321bd90b4fbb289c8a7b
bigstar888 <goldtrump1015@gmail.com>
2021-12-22 20:03:40 +0800

    Minor changes

---
+++ frontend/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <h1>About XMUM</h1>
+  <p>
+    This is the online reservation system.
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+};
+</script>
+
+<style scoped>
+
+</style>
