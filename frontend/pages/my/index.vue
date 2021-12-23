17055087fe5d86f8f202e08a12c34a986ce6d7a0
bigstar888 <goldtrump1015@gmail.com>
2021-12-23 17:51:57 +0800

    fix: combine my/reservations to my/

--- frontend/pages/my/index.vue
+++ frontend/pages/my/index.vue
@@ -1,16 +1,25 @@
 <template>
-<div>
-  // TODO: add login page routing here
-</div>
+  <div>
+    <CustomPageTitle>My Reservation</CustomPageTitle>
+    <MyInfo :user='currentUser' />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'index',
+  name: 'reservation',
   middleware: ['auth'],
+  data() {
+    return {};
+  },
+  computed: {
+    currentUser() {
+      return this.$store.state.user;
+    }
+  }
 };
 </script>
 
-<style scoped>
+<style scoped lang='scss'>
 
 </style>
