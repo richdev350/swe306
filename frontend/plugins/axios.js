424295d701ed8951623cd695196c9c3442501b03
bigstar888 <goldtrump1015@gmail.com>
2021-12-24 00:18:00 +0800

    fix: change $api interceptors

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,24 +1,33 @@
 import Element from 'element-ui';
 import config from '~/service/config';
 
-export default function({ $axios }, inject) {
+export default function ({ $axios }, inject) {
   // Create a custom axios instance
   const api = $axios.create(config);
   // request handler
-  api.onRequest(config => {
-    // Element.Loading.service({
-    //   lock: true,
-    //   text: 'Loading',
-    //   background: 'rgba(0, 0, 0, 0.7)'
-    // });
-    console.log('requesting...');
+  let loadingInstance;
+  api.onRequest((config) => {
+    loadingInstance = Element.Loading.service({
+      lock: true,
+      text: 'Loading',
+      spinner: 'el-icon-loading',
+      background: 'rgba(0, 0, 0, 0.7)',
+    });
+    console.log('api is making request to ' + config.url);
+  });
+
+  api.onResponse((response) => {
+    loadingInstance.close();
   });
 
   // error interceptor
-  api.onError(error => {
+  api.onError((error) => {
+    loadingInstance.close();
     console.log(error);
     const code = parseInt(error.response && error.response.status);
-    Element.Message.error(`Error Message: ${ error.response.data.message }, Error Code:${ code }`);
+    Element.Message.error(
+      `Error Message: ${error.response.data.message}, Error Code:${code}`
+    );
     return Promise.resolve(false);
     // return Promise.reject(error);
   });
