0a5c38d3c89d3179e05777347edddb3173fee7d8
bigstar888 <goldtrump1015@gmail.com>
2021-12-25 20:04:16 +0800

    fix: method to read authToken from Cookie

--- backend/src/main/java/com/peg6/peg6backend/service/AuthenticateServer.java
+++ backend/src/main/java/com/peg6/peg6backend/service/AuthenticateServer.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Service;
 public class AuthenticateServer {
     TokenEntity tokenEntity = new TokenEntity();
 
-    public boolean authenticateToken(String Token){
-        if(Token==null) return false;
+    public boolean authenticateToken(String Token) {
+        if (Token == null) return false;
         String[] tokens = Token.split("=");
-        String token = tokens[tokens.length-1];
+        String token = tokens[tokens.length - 1];
         return token.equals(tokenEntity.token);
     }
 }
