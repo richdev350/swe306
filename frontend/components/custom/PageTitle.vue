326050ab2fdb8630d847af8f2562211e9114e664
bigstar888 <goldtrump1015@gmail.com>
2021-12-25 16:49:49 +0800

    feat: size selectable for CustomPageTitle

--- frontend/components/custom/PageTitle.vue
+++ frontend/components/custom/PageTitle.vue
@@ -1,12 +1,24 @@
 <template>
-  <h2 class='title'>
-    <slot></slot>
-  </h2>
+  <div>
+    <h2 v-if="size === 'h2'" class='title'>
+      <slot></slot>
+    </h2>
+
+    <h3 v-if="size === 'h3'" class='title'>
+      <slot></slot>
+    </h3>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Title'
+  name: 'Title',
+  props: {
+    size: {
+      type: String,
+      default: 'h2'
+    }
+  }
 };
 </script>
 
