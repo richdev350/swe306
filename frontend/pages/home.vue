c91c5ff4aa89217a042a4fa1130147a7325479ac
bigstar888 <goldtrump1015@gmail.com>
2021-12-26 20:32:55 +0800

    fix: home images

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
 
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
+    <el-card class='box-card'>
+      <div slot='header' class='clearfix'>
         <span style='font-size: 25px'>Welcom to XMUM Discussion Room Reservation System. </span>
       </div>
-    <img src='../assets/images/xmum.jpeg' class='card_image'>
-<!--    <div class='test'>SomeText</div>
-    <el-button>
-      <NuxtLink to='/test'>Test</NuxtLink>
-    </el-button>
-    <p>is authed: {{ isAuthenticated }}</p>
-    <p>user: {{ loggedInUser }}</p>
-    <p>cookie: {{ $cookies.getAll() }}</p>
+      <img src='~/assets/images/xmum.jpeg' class='card_image'>
+      <!--    <div class='test'>SomeText</div>
+          <el-button>
+            <NuxtLink to='/test'>Test</NuxtLink>
+          </el-button>
+          <p>is authed: {{ isAuthenticated }}</p>
+          <p>user: {{ loggedInUser }}</p>
+          <p>cookie: {{ $cookies.getAll() }}</p>
 
-    <el-divider></el-divider>
-    <el-button @click='getRoomList'>Get Room List</el-button>
-    <el-button @click='cleanRoomList'>Clean Room List</el-button>
-    {{ roomList }}-->
+          <el-divider></el-divider>
+          <el-button @click='getRoomList'>Get Room List</el-button>
+          <el-button @click='cleanRoomList'>Clean Room List</el-button>
+          {{ roomList }}-->
     </el-card>
     <el-divider content-position='center'>Campus Landscape</el-divider>
-    <el-carousel :interval="4000" type="card" height="300px">
-      <el-carousel-item v-for="item in images" :key="item">
-        <h3 class="medium"> <img :src='item.url' class='image'/></h3>
+    <el-carousel :interval='4000' type='card' height='300px'>
+      <el-carousel-item v-for='item in images' :key='item'>
+        <h3 class='medium'><img :src='require(`~/assets/images/${item.url}`)' class='image' alt='item.desc' /></h3>
       </el-carousel-item>
     </el-carousel>
 
@@ -37,18 +37,23 @@ export default {
   name: 'Home',
   data() {
     return {
-      images:[{
-       url: "../assets/images/1.jpeg",
-      },
+      images: [
         {
-        url:"../assets/images/2.jpeg",
-      },
-        {
-          url:"../assets/images/3.jpeg",
+          url: '1.jpeg',
+          desc: ''
         },
         {
-          url:"../assets/images/4.jpeg",
+          url: '2.jpeg',
+          desc: ''
         },
+        {
+          url: '3.jpeg',
+          desc: ''
+        },
+        {
+          url: '4.png',
+          desc: ''
+        }
       ]
     };
   },
@@ -74,18 +79,21 @@ export default {
 </script>
 
 <style scoped lang='scss'>
-.box-card{
+.box-card {
   margin-bottom: 5%;
-  height:100%;
-  width:100%;
+  height: 100%;
+  width: 100%;
 }
-.card_image{
-  width:100%;
-  height:100%;
+
+.card_image {
+  width: 100%;
+  height: 100%;
 }
+
 .test {
   @apply md:bg-red-700;
 }
+
 .image {
   width: 100%;
   height: 100%;
