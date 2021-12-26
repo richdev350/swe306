f3acd42ce1859472a5ecd5d51179bc5c0996c524
bigstar888 <goldtrump1015@gmail.com>
2021-12-26 20:40:28 +0800

    Update home.vue

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -97,21 +97,22 @@ export default {
 .image {
   width: 100%;
   height: 100%;
+  background-repeat: repeat;
 }
 
 .el-carousel__item h3 {
-  color: #475669;
+
   font-size: 14px;
-  opacity: 0.75;
+  opacity: 0.9;
   line-height: 150px;
   margin: 0;
 }
 
 .el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+
 }
 
 .el-carousel__item:nth-child(2n+1) {
-  background-color: #d3dce6;
+
 }
 </style>
